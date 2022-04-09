fecb7d09daf4877ad977ac1af84851da860be43f
gobisan14 <gobisandex@gmail.com>
2022-04-09 19:46:36 +0530

    Modify electricity account management

--- ElectricityAccountManagement/src/com/ElectricityAccountManagement/service/ElectricityAccountService.java
+++
@@ -1,5 +0,0 @@
-package com.ElectricityAccountManagement.service;
-
-public class ElectricityAccountService {
-
-}
--- ElectricityAccountManagement/src/com/ElectricityAccountManagement/util/DBConnection.java
+++
@@ -1,5 +0,0 @@
-package com.ElectricityAccountManagement.util;
-
-public class DBConnection {
-
-}
