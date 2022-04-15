d79407ed3a421fb2ae125719cc8b90175aae39c4
dinushi <dinushi.ariyasena@outlook.com>
2022-04-15 19:17:10 +0530

    Develop the RESTful API

--- PowerOutageInforming/src/main/java/com/OutageNoticeService.java
+++ PowerOutageInforming/src/main/java/com/OutageNoticeService.java
@@ -1,5 +1,28 @@
 package com;
+import model.OutageNotice;
 
-public class OutageNoticeService {
+//For REST Service
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 
-}
+//For JSON
+import com.google.gson.*;
+
+//For XML
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
+
+@Path("/OutageNotice")
+public class OutageNoticeService
+{
+	OutageNotice noticeObj = new OutageNotice();
+	
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String readNotices()
+	{
+		return "Hello";
+	}
+}	
