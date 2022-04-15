0604cf41f376d460296b8882f554057d6d9c6119
dinushi <dinushi.ariyasena@outlook.com>
2022-04-15 19:23:53 +0530

    Implement the READ operation

--- PowerOutageInforming/src/main/java/com/OutageNoticeService.java
+++ PowerOutageInforming/src/main/java/com/OutageNoticeService.java
@@ -17,12 +17,27 @@ import org.jsoup.nodes.Document;
 public class OutageNoticeService
 {
 	OutageNotice noticeObj = new OutageNotice();
-	
+
 	@GET
 	@Path("/")
 	@Produces(MediaType.TEXT_HTML)
 	public String readNotices()
 	{
-		return "Hello";
+	 return noticeObj.readNotices();
+	}
+	
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertNotice(@FormParam("regionID") String regionID,
+	 @FormParam("group") String group,
+	 @FormParam("Description") String Description,
+	 @FormParam("outageStartTime") String outageStartTime,
+	 @FormParam("outageEndTime") String outageEndTime,
+     @FormParam("informerID") String informerID)
+	{
+	 String output = noticeObj.insertNotice(regionID, group, Description, outageStartTime, outageEndTime, informerID);
+	 return output;
 	}
 }	
