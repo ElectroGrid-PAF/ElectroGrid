3433a8bb9ae7f3b1ab9aca7f92ce67fd56fdb652
Sanjay-dev-ds <sanjay28.js@gmail.com>
2022-04-19 22:02:19 +0530

    Service File documentation

--- ElectricityTariffPlan/src/com/TariffPlanService.java
+++ ElectricityTariffPlan/src/com/TariffPlanService.java
@@ -18,7 +18,7 @@ import org.jsoup.parser.Parser;
 @Path("/Plans")
 public class TariffPlanService {
 	TariffPlan Tobj = new TariffPlan();
-
+// API For Inter-connection for meter reading service
 	@GET
 	@Path("/{id}")
 	@Produces(MediaType.TEXT_PLAIN)
@@ -34,7 +34,7 @@ public class TariffPlanService {
 	@Produces(MediaType.TEXT_PLAIN)
 	public String CreateTariffPlan(@FormParam("ET_ID") String ET_ID, @FormParam("Tariff_Block") String Tariff_Block,
 			@FormParam("Unit_Rate") double Unit_Rate, @FormParam("Fixed_Charge") double Fixed_Charge) {
-
+		   // round to two decimal digits. 
 		double Ur = (double) Math.round(Unit_Rate * 100) / 100;
 		double Fc = (double) Math.round(Fixed_Charge * 100) / 100;
 
@@ -62,7 +62,7 @@ public class TariffPlanService {
 	@Produces(MediaType.TEXT_PLAIN)
 	public String UpdateTariffPlan(@FormParam("ET_ID") String ET_ID, @FormParam("Tariff_Block") String Tariff_Block,
 			@FormParam("Unit_Rate") double Unit_Rate, @FormParam("Fixed_Charge") double Fixed_Charge) {
-
+            // round to two decimal digits. 
 		double Ur = (double) Math.round(Unit_Rate * 100) / 100;
 		double Fc = (double) Math.round(Fixed_Charge * 100) / 100;
 
